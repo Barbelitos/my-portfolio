70388f40acd5f4952f765b36470b7390837cc543
Barbelitos <davidbbf@gmail.com>
2022-04-23 16:40:18 +0300

    Added footer.

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,15 @@
+import SocialLinksContainer from "../UI/SocialLinksContainer";
 import classes from "./Footer.module.css";
 
 const Footer = () => {
-  return <footer className={classes.footer}></footer>;
+  return (
+    <footer className={classes.footer}>
+      <SocialLinksContainer />
+      <h4>
+        David Fonseca <span>©2021</span>
+      </h4>
+    </footer>
+  );
 };
 
 export default Footer;

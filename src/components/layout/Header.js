f661e8f408a81f00ca88e9420ec792f608e91e44
Barbelitos <davidbbf@gmail.com>
2022-04-24 13:11:13 +0300

    Changed CSS rules for header.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,7 +7,6 @@ import background from "../../assets/background.jpg";
 
 const Header = (props) => {
   return (
-<<<<<<< HEAD
     <header
       style={{
         background: `linear-gradient(rgba(9,9,35,0.8), rgba(9,9,35,0.8)), url(${background})`,
@@ -15,9 +14,6 @@ const Header = (props) => {
       id="header-section"
       className={classes.header}
     >
-=======
-    <header style={{ backgroundColor: "rgb(9,9,35)", height: "100vh", paddingTop: "8rem" }} id="header-section" className={classes.header}>
->>>>>>> e3bd928fa36c20c36dd841136bdeb4a8633eba3e
       <Navbar />
       <HomeContainer />
     </header>

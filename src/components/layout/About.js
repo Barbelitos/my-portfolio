a81a370e1dc5e4ef2c8f13e786c4dfb58ea46631
Barbelitos <davidbbf@gmail.com>
2022-04-24 14:25:33 +0300

    Added about section animation for testing.

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -2,10 +2,27 @@ import AboutContainer from "./AboutContainer";
 
 import classes from "./About.module.css";
 
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const About = () => {
+  useEffect(() => {
+    gsap.from("#testAbout", {
+      y: 500,
+      duration: 0.8,
+      delay: 0.2,
+      scrollTrigger: { trigger: "#about-section" },
+    });
+  }, []);
+
   return (
     <section id="about-section" className={classes.about}>
-      <h1 className={classes.about_title}>About</h1>
+      <h1 id="testAbout" className={classes.about_title}>
+        About
+      </h1>
       <AboutContainer />
     </section>
   );

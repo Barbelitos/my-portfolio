0d8ac65a9dd4415006fa79199a75115e7cbb579f
Barbelitos <davidbbf@gmail.com>
2022-04-27 06:12:32 +0300

    Small changes.

--- src/components/Header/HomeContainerButton.js
+++ src/components/Header/HomeContainerButton.js
@@ -1,13 +1,20 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 import classes from "./HomeContainerButton.module.css";
 
 const HomeContainerButton = () => {
   return (
-    <a className={classes.home_link} href="#about-section">
-      <Typography variant="h5">Get to know me </Typography>
-      <ArrowForwardIosIcon fontSize="small" className={classes.button_arrow} />
+    <a href="#about-section">
+      <button className={classes.home_link}>
+        <Typography variant="h5">Get to know me </Typography>
+        <span>
+          <ArrowForwardIosIcon
+            fontSize="small"
+            className={classes.button_arrow}
+          />
+        </span>
+      </button>
     </a>
   );
 };
